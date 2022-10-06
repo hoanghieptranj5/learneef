e5b345c478127e4fb6f39e31e58ca8964f5a4e38
Hiep Tran <hiep.tran@zoi.de>
2022-10-06 21:42:33 +0700

    refactored code

--- src/LearnEF/Controllers/EmployeeController.cs
+++ src/LearnEF/Controllers/EmployeeController.cs
@@ -15,7 +15,7 @@ public class EmployeeController : ControllerBase
     }
     
     // GET api/values
-    [HttpGet("{id}")]
+    [HttpGet("{id:int}")]
     public async Task<IActionResult> Get(int id)
     {
         var result = from order in _dbContext.Orders
@@ -26,7 +26,7 @@ public class EmployeeController : ControllerBase
     }
     
     // GET api/values
-    [HttpGet("{id}/order")]
+    [HttpGet("{id:int}/order")]
     public async Task<IActionResult> GetCustomerOrder(int id)
     {
         var result = from employee in _dbContext.Employees
@@ -35,7 +35,7 @@ public class EmployeeController : ControllerBase
         return Ok(await result.ToListAsync());
     }
 
-    [HttpGet("{id}/something")]
+    [HttpGet("{id:int}/something")]
     public async Task<IActionResult> GetSomething(int id)
     {
         var result = await _dbContext.Employees
